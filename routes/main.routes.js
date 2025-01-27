d6d73a94b28d9f7e93cad15c4a176543275f83f9
sastatesla <itsamaanansari@gmail.com>
2025-01-27 21:46:00 +0530

    boilerplate nosql

--- routes/main.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const UserRouter = require("./user.routes")
-
-router.use("/user", UserRouter)
-
-exports.default = router
--- routes/user.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { register, login } = require('../controllers/user.controllers');
-const router = express.Router();
-
-router.post('/register', register);
-router.post('/login', login);
-
-module.exports = router;
